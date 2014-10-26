00a47e19399ee14031f569589d170a466e012ac8
gitdropcode <dzguangdai@gmail.com>
2014-10-26 09:53:29 -0400

    Added full-screen window (Alt-F4, Cmd-Q to close)

---
+++ src/com/lqtz/globaldomination/GameWindow.java
@@ -0,0 +1,38 @@
+package com.lqtz.globaldomination;
+
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GameWindow extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+	public JFrame frame;
+	
+	public GameWindow()
+	{
+		frame = new JFrame();
+		frame.add(this);
+		frame.setUndecorated(true);
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		if (gd.isFullScreenSupported())
+		{
+			gd.setFullScreenWindow(frame);
+		}
+		else
+		{
+			frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		}
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	public static void main(String[] args)
+	{
+		new GameWindow();
+	}
+
+}
