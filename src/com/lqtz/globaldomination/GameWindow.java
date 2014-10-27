0e3b6d5b7cc082f6fae7125cd48823d76cd4acb3
gitdropcode <dzguangdai@gmail.com>
2014-10-27 14:03:01 -0400

    Changed GameWindow to JFrame

--- src/com/lqtz/globaldomination/GameWindow.java
+++ src/com/lqtz/globaldomination/GameWindow.java
@@ -5,35 +5,31 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class GameWindow extends JPanel
+public class GameWindow extends JFrame
 {
 	private static final long serialVersionUID = 1L;
-	public JFrame frame;
 	
 	public GameWindow()
 	{
-		frame = new JFrame();
-		frame.add(this);
-		frame.setUndecorated(true);
+		setUndecorated(true);
 		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 		if (gd.isFullScreenSupported())
 		{
-			gd.setFullScreenWindow(frame);
+			gd.setFullScreenWindow(this);
 		}
 		else
 		{
-			frame.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+			setSize(Toolkit.getDefaultToolkit().getScreenSize());
 		}
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		addComponents();
+		setVisible(true);
 		
-		init();
 		start();
 	}
 	
-	private void init()
+	private void addComponents()
 	{
 		
 	}
