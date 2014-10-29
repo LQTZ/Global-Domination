6300f0c754e789debf356cc74dbb65dffd0e16ff
Gandalf <ryantse@verizon.net>
2014-10-28 22:28:41 -0400

    Setup buttons and combat odds panes with GridBagLayout, refactored lots of var names

--- src/com/lqtz/globaldomination/GameWindow.java
+++ src/com/lqtz/globaldomination/GameWindow.java
@@ -1,12 +1,16 @@
 package com.lqtz.globaldomination;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -24,17 +28,17 @@ public class GameWindow extends JFrame
 {
 	private static final long serialVersionUID = 1L;
 	
-	// Declares components
-	private JPanel left;
-	private JTextPane units;
-	private JTextPane eventLog;
-	private JPanel center;
-	private JTextPane infoPane;
-	private GameScreen gameScreen;
-	private JPanel control;
-	private JPanel buttonsCont;
-	private JButton[] buttons;
-	private JLabel infoBox;
+	// Declare components
+	private JPanel leftPanel;			// Panel with units info pane and event log pane
+	private JTextPane unitsPane;		// Units info pane
+	private JTextPane eventLogPane;		// Event log pane
+	private JPanel centerPanel;			// Panel with map pane, action buttons pane, and combat info pane
+	private GameScreen mapPane;			// Map pane
+	private JPanel controlPane;		// Pane with buttons pane and combat odds pane
+	private JPanel buttonsPane;			// Pane with action buttons
+	private JButton[] buttons;			// Action buttons themselves
+	private JLabel combatOddsPane;		// Pane with combat odds info
+	private JTextPane infoPanel;		// Pane with tile, city, and game info
 	
 	public GameWindow()
 	{
@@ -52,7 +56,7 @@ public class GameWindow extends JFrame
 			setSize(Toolkit.getDefaultToolkit().getScreenSize());
 		}
 		
-		// Set attributes
+		// Setup screen attributes
 		setDefaultCloseOperation(EXIT_ON_CLOSE); // Ends the program when closed
 		setTitle("Global Domination");
 		addComponents();
@@ -66,48 +70,77 @@ public class GameWindow extends JFrame
 	 */
 	private void addComponents()
 	{
-		// FIXME components not showing up
 		// Left components
-		left = new JPanel(new BorderLayout());
-		units = new JTextPane();
-		units.setPreferredSize(new Dimension(200, getHeight()/2));
-		eventLog = new JTextPane();
-		eventLog.setPreferredSize(new Dimension(200, getHeight()/2));
-		left.add(units, BorderLayout.NORTH);
-		left.add(eventLog, BorderLayout.SOUTH);
+		leftPanel = new JPanel(new BorderLayout());
+		unitsPane = new JTextPane();
+		unitsPane.setPreferredSize(new Dimension(200, getHeight()/2));
+		eventLogPane = new JTextPane();
+		eventLogPane.setPreferredSize(new Dimension(200, getHeight()/2));
+		leftPanel.add(unitsPane, BorderLayout.NORTH);
+		leftPanel.add(eventLogPane, BorderLayout.SOUTH);
 		
 		// Center components
-		center = new JPanel(new BorderLayout());
-		gameScreen = new GameScreen();
-		control = new JPanel(new BorderLayout());
+		centerPanel = new JPanel(new BorderLayout());
+		mapPane = new GameScreen();
+		controlPane = new JPanel(new BorderLayout());
 		
-		// Creates buttons
-		buttonsCont = new JPanel();
-		buttonsCont.setLayout(new BoxLayout(buttonsCont, BoxLayout.LINE_AXIS));
+		// Creates buttons and add them to the buttonsPane
+		buttonsPane = new JPanel();
+		buttonsPane.setLayout(new BoxLayout(buttonsPane, BoxLayout.LINE_AXIS));
 		buttons = new JButton[5];
 		for (int i = 0; i < 5; i++)
 		{
-			buttons[i] = new JButton();
-			buttonsCont.add(buttons[i]);
-			buttons[i].setAlignmentX(Component.CENTER_ALIGNMENT);
-			buttons[i].setAlignmentY(Component.CENTER_ALIGNMENT);
+			buttons[i] = new JButton();		// Create new button
+			buttonsPane.add(buttons[i]);	// Add button
+			buttons[i].setAlignmentX(Component.CENTER_ALIGNMENT);		// Align button
+			buttons[i].setAlignmentY(Component.CENTER_ALIGNMENT);		//
 		}
-		buttonsCont.setPreferredSize(new Dimension(getWidth() - 400, 300));
+		buttonsPane.setPreferredSize(new Dimension(getWidth() - 400, 300));
 		
-		infoBox = new JLabel();
-		infoBox.setPreferredSize(new Dimension(getWidth() - 400, 50));
-		control.add(buttonsCont, BorderLayout.NORTH);
-		control.add(infoBox, BorderLayout.SOUTH);
-		center.add(gameScreen, BorderLayout.NORTH);
-		center.add(control, BorderLayout.SOUTH);
+		// Set up the buttons panel with GridBagLayout
+		// TODO Make buttons panel distributed across the entire center panel
+		controlPane.setLayout(new GridBagLayout());		// Change controlPane layout setter to GridBagLayout
+		GridBagConstraints controlPanelFormattingConstraints = new GridBagConstraints();	// Object to hold GridBagLayout constraints
+		controlPanelFormattingConstraints.fill = GridBagConstraints.HORIZONTAL;
+		controlPanelFormattingConstraints.anchor = GridBagConstraints.NORTH;
+		controlPanelFormattingConstraints.gridx = 3;       // Centered (aligned at column 3)
+		controlPanelFormattingConstraints.gridwidth = 5;   // 5 columns wide
+		controlPanelFormattingConstraints.gridy = 1;       // First row
+		controlPane.add(buttonsPane, controlPanelFormattingConstraints);	// Add the buttons pane
 		
-		// Right components
-		infoPane = new JTextPane();
-		infoPane.setPreferredSize(new Dimension(200, getHeight()));
+		// Set up the combat odds label and pane to go below the action buttons
+		// TODO Make combat odds panel distributed across the entire center panel
+		combatOddsPane = new JLabel();
+		combatOddsPane.setPreferredSize(new Dimension(getWidth() - 400, 50));
+		controlPanelFormattingConstraints.anchor = GridBagConstraints.SOUTH;
+		controlPanelFormattingConstraints.gridx = 3;       // Centered (aligned at column 3)
+		controlPanelFormattingConstraints.gridwidth = 5;   // 5 columns wide
+		controlPanelFormattingConstraints.gridy = 2;       // First row
+		controlPane.add(combatOddsPane, controlPanelFormattingConstraints);	// Add the combat odds pane
 		
-		add(left, BorderLayout.WEST);
-		add(center, BorderLayout.CENTER);
-		add(infoPane, BorderLayout.EAST);
+		// Add Containers to the main center panel
+		centerPanel.add(mapPane, BorderLayout.NORTH);
+		centerPanel.add(controlPane, BorderLayout.SOUTH);
+		
+		// Add Containers to the main right components (only the info panel)
+		infoPanel = new JTextPane();
+		infoPanel.setPreferredSize(new Dimension(200, getHeight()));
+		
+		// Outline panels with color (to be removed)
+		leftPanel.setBorder(BorderFactory.createLineBorder(Color.black));			// Main panels black
+		centerPanel.setBorder(BorderFactory.createLineBorder(Color.black));			//
+		infoPanel.setBorder(BorderFactory.createLineBorder(Color.black));			//
+		mapPane.setBorder(BorderFactory.createLineBorder(Color.blue));				// Map blue
+		unitsPane.setBorder(BorderFactory.createLineBorder(Color.cyan));			// Units pane cyan
+		eventLogPane.setBorder(BorderFactory.createLineBorder(Color.pink));		// Event log pane pink
+		controlPane.setBorder(BorderFactory.createLineBorder(Color.blue));			// Control pane blue
+		buttonsPane.setBorder(BorderFactory.createLineBorder(Color.cyan));			// Buttons pane cyan
+		combatOddsPane.setBorder(BorderFactory.createLineBorder(Color.green));	// Combat odds pane green
+		
+		// Add the main panels
+		add(leftPanel, BorderLayout.WEST);
+		add(centerPanel, BorderLayout.CENTER);
+		add(infoPanel, BorderLayout.EAST);
 	}
 	
 	/**
