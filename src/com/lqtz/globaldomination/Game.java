5ed84183e9855e5c54a332ac8ef98e036c861391
gitdropcode <dzguangdai@gmail.com>
2014-11-18 11:31:17 -0500

    Encapsulated fonts in Game, but broke something and screen doesnt display

--- src/com/lqtz/globaldomination/Game.java
+++ src/com/lqtz/globaldomination/Game.java
@@ -1,15 +1,19 @@
 package com.lqtz.globaldomination;
 
+import java.awt.FontFormatException;
 import java.io.IOException;
 
+import com.lqtz.globaldomination.io.Fonts;
 import com.lqtz.globaldomination.io.Images;
 
 public class Game
 {
 	public Images images;
-	
+	public Fonts fonts;
+
 	public Game()
 	{
+		// Import images
 		try
 		{
 			images = new Images();
@@ -17,6 +21,22 @@ public class Game
 		catch (IOException e)
 		{
 			System.err.println("Images not found");
+			e.printStackTrace();
+		}
+
+		// Initializes fonts
+		try
+		{
+			fonts = new Fonts();
+		}
+		catch (FontFormatException e)
+		{
+			System.err.println("Fonts corrupted");
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			System.err.println("Fonts not found");
 			e.printStackTrace();
 		}
 		new GameWindow(this);
